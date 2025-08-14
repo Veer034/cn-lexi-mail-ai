c9949a1da497edca20675c19cb92e6d999e020b8
Veer034 <ransingh92@yahoo.com>
2025-08-15 00:14:02 +0530

    verify false

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ ES_CONFIG = {
     'username': os.getenv('ES_USERNAME',''),
     'password': os.getenv('ES_PASSWORD',''),
     'ca_certs': '/usr/local/share/ca-certificates/elasticsearch.crt',  # Point to certificate file
-    'verify_certs': True,
+    'verify_certs': False,
     'ssl_show_warn': True,
     'tenant_document_index_name': os.getenv('ES_TENANT_DOCUMENTS_VECTOR_INDEX_NAME', 'tenant-documents-vector')
 }
